6ea53c87c6bd6170a5d8adce1cc4f4c3b90df0e7
mozhaa <mozhay2005@gmail.com>
2025-04-30 16:51:47 +0300

    bug fix

--- src/sampling.cpp
+++ src/sampling.cpp
@@ -13,7 +13,7 @@ static inline glm::vec3 from_spherical(float theta, float phi, glm::vec3 normal)
 
 true_uniform_sampler::true_uniform_sampler(std::minstd_rand0 &rng) : sampler(rng) {}
 
-std::pair<glm::vec3, float> true_uniform_sampler::sample(glm::vec3 normal) const {
+std::pair<glm::vec3, float> true_uniform_sampler::sample(glm::vec3 normal) {
     std::normal_distribution<float> d(0.f, 1.f);
     glm::vec3 v = {d(rng), d(rng), d(rng)};
     v = glm::normalize(v);
@@ -25,7 +25,7 @@ std::pair<glm::vec3, float> true_uniform_sampler::sample(glm::vec3 normal) const
 
 uniform_sampler::uniform_sampler(std::minstd_rand0 &rng) : sampler(rng) {}
 
-std::pair<glm::vec3, float> uniform_sampler::sample(glm::vec3 normal) const {
+std::pair<glm::vec3, float> uniform_sampler::sample(glm::vec3 normal) {
     std::uniform_real_distribution<float> d(0.f, 1.f);
     float e1 = d(rng), e2 = d(rng);
     float theta = std::acos(e1);
@@ -35,7 +35,7 @@ std::pair<glm::vec3, float> uniform_sampler::sample(glm::vec3 normal) const {
 
 cosine_sampler::cosine_sampler(std::minstd_rand0 &rng) : sampler(rng), d(0.f, 1.f) {}
 
-std::pair<glm::vec3, float> cosine_sampler::sample(glm::vec3 normal) const {
+std::pair<glm::vec3, float> cosine_sampler::sample(glm::vec3 normal) {
     float e1 = d(rng), e2 = d(rng);
     float theta = std::acos(std::sqrt(e1));
     float phi = glm::two_pi<float>() * e2;
@@ -44,7 +44,7 @@ std::pair<glm::vec3, float> cosine_sampler::sample(glm::vec3 normal) const {
 
 power_cosine_sampler::power_cosine_sampler(std::minstd_rand0 &rng, float alpha) : sampler(rng), alpha(alpha) {}
 
-std::pair<glm::vec3, float> power_cosine_sampler::sample(glm::vec3 normal) const {
+std::pair<glm::vec3, float> power_cosine_sampler::sample(glm::vec3 normal) {
     std::uniform_real_distribution<float> d(0.f, 1.f);
     float e1 = d(rng), e2 = d(rng);
     float theta = std::acos(std::pow(e1, 1.f / (1.f + alpha)));
